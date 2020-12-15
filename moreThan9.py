a5e921a9e9af3793c3279db8d2caee2b0478686a
Rabya Tayal <69662484+RDalie@users.noreply.github.com>
2020-12-15 16:17:32 +0530

    Add files via upload
    numbers in Indian system --- arab, kharab, crore, etc.

--- moreThan9.py
+++ moreThan9.py
@@ -5,158 +5,127 @@ number_dict = {'0':'zero','1':'one','2':'two','3':'three','4':'four','5':'five',
 '17':'seventeen','18':'eighteen','19':'nineteen','20':'twenty','30':'thirty','40':'fourty','50':'fifty','60':'sixty',
 '70':'seventy','80':'eighty','90':'ninety','100':'one hundred'}
 
+def number_to_word(pos,number='0',list=None,add_zero=False,string=''):
+    if number[pos]!='0': #getting words only if the number is non-zero
+        if add_zero:
+            num=number[pos]+'0'
+        else:
+            num=number[pos]
+        num_words=number_dict[num] + string
+        list.append(num_words)
+
+def teen_check(pos2,pos1,number='0',list=None,string=''):
+        num=number[pos2]+number[pos1]
+        num_words=number_dict[num] + string
+        list.append(num_words)
+
 while True:
-    number_input = input('\nWrite the number:')
+    number = input('\nWrite the number:')
 
     #for numbers with commas
-    if ',' in number_input:
-        number_input=number_input.replace(',','')
+    if ',' in number:
+        number=number.replace(',','')
 
     #sanity check
     try:
-        int(number_input)
+        int(number)
     except:
         print('Put a valid number!')
         continue
 
-    #checks for an input with starting value 0,multiple 0s like 0000 or 00 or 00
-    number_input=number_input.lstrip('0')
-    if len(number_input)<1:
+    #checks for an input with starting value 0,
+    number=number.lstrip('0')
+
+    if len(number) >13:
+        print('Put a valid number!')
+        continue
+
+    #checks for an input with multiple 0s like 0000 or 00 or 00
+
+    if len(number)<1:
         print('zero')
         continue
     else:
          pass
 
-    #dividing a number into two parts if greater than 9 digits
-    parts=list()
-    if len(number_input)>9:
-        number_part1=number_input[-7:]
-        parts.append(number_part1)
-        number_part2=number_input[:-7]
-        parts.append(number_part2)
-    else:
-        parts.append(number_input)
-    # print(number_part1,number_part2)
-
     words=list()
-    number_len=len(number_input)
-    times=1
-    # print(parts)
-    #looping through 1 or 2 parts
-    for number in parts:
-        # print(number)
-        #checking if a divided number consists of zeroes only
-        number=number.lstrip('0')
-        if len(number)<1:
-            times=times+1
-            continue
-        else:
-             pass
+    number_len=len(number)
 
-        #word check for the 2nd part of the number
-        if times==2:
-            words.append('crore')
+    #checks if the number is already present in our dictionary otherwise passes on for more processing
+    try:
+        words.append(number_dict[number])
+        found=True
+    except:
+        found=False
+        pass
 
-        #checks if the number is already present in our dictionary otherwise passes on for more processing
-        try:
-            words.append(number_dict[number])
-            found=True
-            continue
-        except:
-            found=False
-            pass
+    #word assigning code block
+    if not found:
+        while True:
+            # print(len(number))
+            try: #checks if the tens and ones place is a teen number
+                teen_check(-2,-1,number,words)
+                if number_len == 2:break
+            except:
+                number_to_word(-1,number,words)
+                number_to_word(-2,number,words,True)
+                if number_len ==2:break #for two digit number
 
+            number_to_word(-3,number,words,False,' hundred')
+            if number_len ==3:break#for three digit number
 
-        number_len=len(number)
-        #word assigning code block
-        if not found:
-            while True:
-                # print(len(number))
-                try: #checks if the tens and ones place is a teen number
-                    ones=number[-2]+number[-1]
-                    ones_words=number_dict[ones]
-                    words.append(ones_words)
-                    if number_len == 2:break
-                except:
-                    if number[-1]!='0': #getting words only if the number is non-zero
-                        ones=number[-1]
-                        ones_words=number_dict[ones]
-                        words.append(ones_words)
+            try: #checks if the  ten-thousandth and thousandth place is a teen number
+                teen_check(-5,-4,number,words,' thousand')
+                if number_len == 5:break
 
-                    if number[-2]!='0':
-                        tens=number[-2]+'0'
-                        tens_words=number_dict[tens]
-                        words.append(tens_words)
-                        if number_len ==2:break #for two digit number
+            except:
+                number_to_word(-4,number,words,False,' thousand')
+                if number_len ==4:break #for four digit number
+                number_to_word(-5,number,words,True)
+                if number_len ==5:break #for five digit number
 
-                if number[-3]!='0':
-                    hundreds=number[-3]
-                    hundreds_words=number_dict[hundreds]+' hundred'
-                    words.append(hundreds_words)
-                    if number_len ==3:break#for three digit number
+            try: #checks if the  ten-lakhth and lakhth place is a teen number
+                teen_check(-7,-6,number,words,' lakh')
+                if number_len == 7:break
 
-                try: #checks if the  ten-thousandth and thousandth place is a teen number
-                    ten_thousands=number[-5]+number[-4]
-                    ten_thousands_words=number_dict[ten_thousands] + ' thousand'
-                    words.append(ten_thousands_words)
-                    if number_len == 5:break
-
-                except:
-                    if number[-4]!='0':
-                        thousands=number[-4]
-                        thousands_words=number_dict[thousands]  + ' thousand'
-                        words.append(thousands_words)
-                        if number_len ==4:break #for four digit number
-
-                    if number[-5]!='0':
-                        ten_thousands=number[-5] + '0'
-                        ten_thousands_words=number_dict[ten_thousands]
-                        words.append(ten_thousands_words)
-                        if number_len ==5:break #for five digit number
-
-                try: #checks if the  ten-lakhth and lakhth place is a teen number
-                    ten_lakhs=number[-7]+number[-6]
-                    ten_lakhs_words=number_dict[ten_lakhs] + ' lakh'
-                    words.append(ten_lakhs_words)
-                    if number_len == 7:break
-
-                except:
-                    if number[-6]!='0':
-                        lakhs=number[-6]
-                        lakhs_words=number_dict[lakhs] + ' lakh'
-                        words.append(lakhs_words)
-                        if number_len ==6: break #for six digit number
-
-                    if number[-7]!='0':
-                        ten_lakhs=number[-7]+'0'
-                        ten_lakhs_words=number_dict[ten_lakhs]
-                        words.append(ten_lakhs_words)
-                        if number_len ==7: break #for seven digit number
+            except:
+                number_to_word(-6,number,words,False,' lakh')
+                if number_len ==6: break #for six digit number
+                number_to_word(-7,number,words,True)
+                if number_len ==7: break #for seven digit number
 
 
+            try: #checks if the  ten-croreth and croreth place is a teen number
+                teen_check(-9,-8,number,words,' crore')
+                if number_len == 9:break
 
-                try: #checks if the  ten-croreth and croreth place is a teen number
-                    ten_crores=number[-9]+number[-8]
-                    ten_crores_words=number_dict[ten_crores] + ' crore'
-                    words.append(ten_crores_words)
-                    if number_len == 9:break
+            except:
+                number_to_word(-8,number,words,False,' crore')
+                if number_len ==8: break #for eight digit number
+                number_to_word(-9,number,words,True)
+                if number_len ==9: break #for nine digit number
 
-                except:
-                    if number[-8]!='0':
-                        crores=number[-8]
-                        crore_words=number_dict[crores] + ' crore'
-                        words.append(crore_words)
-                        if number_len ==8: break #for eight digit number
+            try:
+                teen_check(-11,-10,number,words,' arab')
+                if number_len == 11 : break
+            except:
+                number_to_word(-10,number,words,False,' arab')
+                if number_len== 10: break
+                number_to_word(-11,number,words,True)
+                if number_len== 11: break
 
-                    if number[-9]!='0':
-                        ten_crores=number[-9]+'0'
-                        ten_crores_words=number_dict[ten_crores]
-                        words.append(ten_crores_words)
-                        if number_len ==9: break #for nine digit number
+            try:
+                teen_check(-13,-12,number,words,' kharab')
+                if number_len==13:break
+            except:
+                number_to_word(-12,number,words,False,' kharab')
+                if number_len==12:break
+                number_to_word(-13,number,words,True)
+                if number_len == 13:break
 
-        times=times+1
 
     pos=-1
+    # print(words)
     for times in range(len(words)):
         print(words[pos],end=' ')
 
